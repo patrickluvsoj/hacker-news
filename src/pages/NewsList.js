f5d30fdbf061e62765ae597c2dc65e82359f3229
patrickluvsoj <patricklovesoj@gmail.com>
2021-12-13 19:46:22 -0800

    added news item api fetch

--- src/pages/NewsList.js
+++ src/pages/NewsList.js
@@ -8,10 +8,25 @@ function NewsList(props) {
         console.log("loading news list for: " + props.selected)
     }, [props.selected])
 
-    async function fetchNews(selected) {
+    async function fetchId(selected) {
         const response = await fetch(`https://hacker-news.firebaseio.com/v0/${selected}.json?print=pretty`)
         const jsonObj = await response.json()
-        setNewsList(jsonObj.map(news => <li>{news}</li>))
+        //setNewsList(jsonObj.map(news => <li>{news}</li>))
+        const items = jsonObj.map(id => {
+            return fetchNews(id)
+        })
+
+        return items.maps(item => {
+            <li key={item.id}>
+                <a href={item.url}>{item.title}</a>
+                by {item.by} | {item.time} | {item.score}
+            </li>
+        })
+    }
+
+    async function fetchNews(id) {
+        const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+        return await response.json()
     }
     
     return (
