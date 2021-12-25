0fe18d2a92bb93c7b2782613bc02364c4c961497
patrickluvsoj <patricklovesoj@gmail.com>
2021-12-25 11:26:18 -0800

    added expand comments sections

---
+++ src/pages/Comments.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react'
+import {MdExpandMore, MdExpandLess} from "react-icons/md";
+import {IconContext} from "react-icons";
+
+function ExpandIcon(props) {
+    if (props.isActive) {
+        return (
+            <IconContext.Provider value={{ style: {fontSize: '24px', color: "rgb(202 138 4)"}}}>
+                <MdExpandMore />
+            </IconContext.Provider>
+        )
+    } else {
+        return (
+            <IconContext.Provider value={{ style: {fontSize: '24px', color: "rgb(202 138 4)"}}}>
+                <MdExpandLess />
+            </IconContext.Provider>
+        )
+    }
+}
+
+function Comments(props) {
+
+    const [isActive, setActive] = useState(props.isActive)
+    const [commentList, setComments] = useState("")
+
+    useEffect(() => {
+        console.log(props.commentIds)
+        fetchComments(props.commentIds)
+    }, [])
+
+    async function fetchComments(commentIds) {
+        if (commentIds) {
+            const jsonComments = await Promise.all(commentIds.slice(0,5).map(id => {
+                return fetch(`https://hacker-news.firebaseio.com/v0/item/${id.toString()}.json?print=pretty`)
+                        .then(comment => comment.json())
+            }))
+    
+            const commentList = jsonComments.map(comment => {
+                return (
+                    <li key={comment.id} >
+                        <label className={"ml-1 mb-2 text-sm text-yellow-700"}>{"By " + comment.by + ": "}</label>
+                        <div className={"ml-3 mb-2 text-sm text-yellow-700"} dangerouslySetInnerHTML={{__html: comment.text}} />
+                    </li>
+                    
+                )
+    
+            })
+
+            setComments(commentList)
+
+        } else {
+            const commentList = <li className={"ml-3 mb-2 text-sm text-yellow-700"}>"no comments"</li>
+            setComments(commentList)
+        }        
+    }
+
+    const handleClick = async () => {
+        setActive(!isActive)
+    }
+    
+    return(
+        <div>
+            <button classname={"justify-self-end"} onClick={handleClick}>
+                <ExpandIcon isActive={isActive}/>
+            </button>
+            <ul className={isActive ? "" : "hidden"}>
+                {commentList}
+            </ul>
+        </div>
+
+    )
+}
+
+export default Comments
--- src/pages/NewsList.js
+++ src/pages/NewsList.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import Comments from './Comments'
 
 function NewsList(props) {
     const [newsList, setNewsList] = useState([])
@@ -24,14 +25,19 @@ function NewsList(props) {
             const hr = d.getHours()
             const min = d.getMinutes()
 
+            console.log(item?.kids)
+
             return (
                 <div className={"m-2 p-2 border rounded border-gray-400"}>
-                    <li className={"grid-rows-2"} key={item?.id}>
+                    <li className={"grid-rows-3"} key={item?.id}>
                         <div>
                             <a className={"text-lg text-yellow-600"} href={item?.url}>{item?.title}</a>
                         </div>
                         <div>
-                            <label className={"ml-1 text-sm text-yellow-700"}>by {item?.by} | {mon}/{day}/{yr}, {hr > 12 ? hr - 12 : hr}:{min} {hr > 12 ? "PM" : "AM"} | {item?.score} comments</label>
+                            <label className={"ml-1 text-sm text-yellow-700"}>by {item?.by} | {mon}/{day}/{yr}, {hr > 12 ? hr - 12 : hr}:{min} {hr > 12 ? "PM" : "AM"} | {item?.descendants} comments</label>
+                        </div>
+                        <div>
+                            <Comments commentIds={item?.kids} isActive={false}/>
                         </div>
                     </li>
                 </div>
